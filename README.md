22dc5f72acd328d170f54563b315c4d89145ca83
mainHossein <ho3eincheraghi20.06@gmail.com>
2026-03-23 15:13:31 +0330

    starting ipg service project

---
+++ README.md
@@ -0,0 +1,5 @@
+# IPG(Internet Payment Gateway) service
+Service related to payment management
+
+base url: http://localhost:8081/api/v1/product
+
---
+++ src/main/java/com/example/ipgserviceproject/IpgServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ipgserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpgServiceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpgServiceProjectApplication.class, args);
+    }
+
+}

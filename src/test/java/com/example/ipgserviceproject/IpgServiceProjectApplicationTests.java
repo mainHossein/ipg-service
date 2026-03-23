ef4c725e61b5a11ac070ab356b52945796548435
mainHossein <ho3eincheraghi20.06@gmail.com>
2026-03-23 15:13:31 +0330

    starting ipg service project

---
+++ src/test/java/com/example/ipgserviceproject/IpgServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ipgserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = IpgServiceProjectApplication.class)
+class IpgServiceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
